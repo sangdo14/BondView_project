63b6c97bc768290b0020d9bfea217cf858044828
sangdo14 <sangdo14@naver.com>
2025-04-11 17:47:26 +0900

    feat: api 테스트

--- js/api_test.js
+++ js/api_test.js
@@ -1,13 +1,14 @@
 const serviceKey = 'VRfOolZbCzTwjY9lqgLbTRXn6oRYkJj%2BQRBuBJX8C3OMuFTpT5r0ND57XoBmfbEuZbqM14cNvYJkZzjufmeXbg%3D%3D';
-const pageNo = 1;
-const numOfRows = 3;
-const basDt = 20250401;
-const resultType = 'json';
-const base_url = 'https://apis.data.go.kr/1160100/service/GetBondSecuritiesInfoService/getBondPriceInfo';
-const req_url = `${base_url}?serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}&resultType=${resultType}&basDt=${basDt}`;
+// Qt
+const pageNoQt = 1;
+const numOfRowsQt = 3;
+const basDtQt = 20250401;
+const resultTypeQt = 'json';
+const baseUrlQt = 'https://apis.data.go.kr/1160100/service/GetBondSecuritiesInfoService/getBondPriceInfo';
+const reqUrlQt = `${baseUrlQt}?serviceKey=${serviceKey}&pageNo=${pageNoQt}&numOfRows=${numOfRowsQt}&resultType=${resultTypeQt}&basDt=${basDtQt}`;
 
 
-fetch(req_url)
+fetch(reqUrlQt)
   .then(data => {
     return data.json();
   })
@@ -18,7 +19,28 @@ fetch(req_url)
     bondData.forEach((data, index)=>{
       console.log('ISIN코드',data.isinCd)
       console.log('종목명',data.itmsNm)
-      console.log('종목명',data.itmsNm)
+    })
+  });
 
+
+  // Pb
+const pageNoPb = 1;
+const numOfRowsPb = 3;
+const basDtPb = 20250401;
+const resultTypePb = 'json';
+const baseUrlPb = 'http://apis.data.go.kr/1160100/service/GetBondIssuInfoService/getBondBasiInfo';
+const reqUrlPb = `${baseUrlPb}?serviceKey=${serviceKey}&pageNo=${pageNoPb}&numOfRows=${numOfRowsPb}&resultType=${resultTypePb}&basDt=${basDtPb}`;
+
+fetch(reqUrlPb)
+  .then(data => {
+    return data.json();
+  })
+  .then(json => {
+    console.log(json.response.body.items.item)
+    const bondData = json.response.body.items.item;
+
+    bondData.forEach((data, index)=>{
+      console.log('ISIN코드',data.isinCd)
+      console.log('종목명',data.itmsNm)
     })
   });
